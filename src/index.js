4afcc8c5a39862b1efd5e74b455063c7d81d64f9
Jon Dewi Jones <jondewijones@gmail.com>
2018-05-30 12:18:16 +0100

    Add react-router and routing rules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './index.css';
 import './scss/index.scss';
 import Preview from './views/Preview';
 import registerServiceWorker from './registerServiceWorker';
+import NavBar from './global/Navbar';
+import DatasetList from './views/DatasetList';
 
-ReactDOM.render(<Preview />, document.getElementById('root'));
+//ReactDOM.render(<Preview />, document.getElementById('root'));
+
+ReactDOM.render((
+    <div>
+        <NavBar />
+        <Router>
+            <span>
+                <Route exact path="/" component={DatasetList} />
+                <Route exact path="/preview" component={Preview} />
+            </span>
+        </Router>
+    </div>
+    ), document.getElementById('root'))
+
 registerServiceWorker();
