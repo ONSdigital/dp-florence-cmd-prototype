ca994785c03fa2e5768ca27ab7245444af8aaa2c
Jon Dewi Jones <jondewijones@gmail.com>
2018-05-30 15:53:17 +0100

    Add new routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './index.css';
 import './scss/index.scss';
@@ -8,6 +8,8 @@ import Preview from './views/Preview';
 import registerServiceWorker from './registerServiceWorker';
 import NavBar from './global/Navbar';
 import DatasetList from './views/DatasetList';
+import DatasetOptions from './views/DatasetOptions';
+import DatasetMetadata from './views/DatasetMetadata';
 
 //ReactDOM.render(<Preview />, document.getElementById('root'));
 
@@ -16,8 +18,10 @@ ReactDOM.render((
         <NavBar />
         <Router>
             <span>
-                <Route exact path="/" component={DatasetList} />
-                <Route exact path="/preview" component={Preview} />
+                <Route exact path={`/`} component={DatasetList} />
+                <Route exact path={`/dataset-options`} component={DatasetOptions} />
+                <Route exact path={`/dataset-metadata`} component={DatasetMetadata} />
+                <Route exact path={`/preview`} component={Preview} />
             </span>
         </Router>
     </div>
