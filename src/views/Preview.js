a80c092584b5e576fe889f2598f4ea949e6e5e8f
Richard-95 <richillwest@gmail.com>
2018-05-30 16:41:32 +0100

    fix margin and Save and submit

--- src/views/Preview.js
+++ src/views/Preview.js
@@ -5,11 +5,11 @@ class Preview extends Component {
   render() {
     return (
         <form className="margin-left--3 margin-right--3">
-        <h1 className="wrapper margin-top--6">Preview</h1>
-         ◀ <a className="btn btn--link" href="http://google.com">Back</a>
-        <iframe src="http://localhost:3000/Consumer-price-inflation.html" title="test" width="100%" height="500px"></iframe>
-        <button type="submit" className="btn btn--primary margin-bottom--1 margin-left--2" >Save</button>
-        <span className="margin-left--1"><a href="http://google.com" id="submit-for-review" className="btn btn--positive">Save and submit for review</a></span>
+        <h1 className="wrapper margin-top--1 margin-left--1">Preview</h1>
+        <div className="margin-left--1"> ◀ <a className="btn btn--link " href="http://google.com">Back</a></div>
+        <iframe className="margin-bottom--1 margin-top--1"src="http://localhost:3000/Consumer-price-inflation.html" title="test" width="100%" height="600px"></iframe>
+        <button type="submit" className="btn btn--primary margin-bottom--1 margin-left--1" >Save</button>
+        <span className="margin-left--1"><a href="/Florence.html" id="submit-for-review" className="btn btn--positive">Save and submit for review</a></span>
         </form>
         );
     }
