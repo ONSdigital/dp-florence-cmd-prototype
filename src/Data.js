57f85756b2679f4574ced2b41eabbe75e882e003
Jon Dewi Jones <jondewijones@gmail.com>
2018-06-04 11:53:07 +0100

    Change ordering

--- src/Data.js
+++ src/Data.js
@@ -2,11 +2,6 @@ export default class Data {
     constructor() {
         this.data = [
             { 
-                name: "Consumer Prices Index including owner occupiers' housing costs (CPIH)",
-                lastUpdated: "Updated today at 8:44am",
-                id: "cpih"
-            },
-            { 
                 name: "Annual Summary of Hours Worked, Place of Work by Local Authority",
                 lastUpdated: "Updated yesterday at 3:15pm",
                 id: "ashe"
@@ -15,6 +10,11 @@ export default class Data {
                 name: "Population estimates for UK, England and Wales, Scotland and Northern Ireland",
                 lastUpdated: "Updated 16 May 2018 at 5:45pm",
                 id: "popest"
+            },
+            { 
+                name: "Consumer Prices Index including owner occupiers' housing costs (CPIH)",
+                lastUpdated: "Updated 8 May 2018 at 8:44am",
+                id: "cpih"
             },
             { 
                 name: "International Passenger Survey 4.01, citizenship group by sex by age by country of last or next residence",
