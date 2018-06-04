40ab8f8919078819b8cf89e98167f8cf0d3e008a
Jon Dewi Jones <jondewijones@gmail.com>
2018-06-04 13:26:54 +0100

    Update iframe to new preview location

--- src/views/Preview.js
+++ src/views/Preview.js
@@ -13,7 +13,7 @@ class Preview extends Component {
                     <h1 className="margin-top--1 margin-bottom--1">Preview</h1>
                 </div>
             </div>
-            <iframe src="../Consumer-Prices-Index.htm" title="ONS website preview" className="iframe--preview" width="100%" height="80vh"></iframe>
+            <iframe src="../preview/consumer-prices-index.html" title="ONS website preview" className="iframe--preview" width="100%" height="80vh"></iframe>
             <div className="grid grid--justify-center">
                 <div className="grid__col-xs-10 grid__col-md-8 grid__col-lg-6">
                     <div className="margin-top--1">
