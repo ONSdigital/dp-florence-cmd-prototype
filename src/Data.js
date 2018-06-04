59da8d86d1215d85bb3bd3af8f098aa67e0f22ed
Jon Dewi Jones <jondewijones@gmail.com>
2018-06-04 08:32:33 +0100

    Add data class and get methods

---
+++ src/Data.js
@@ -0,0 +1,43 @@
+export default class Data {
+    constructor() {
+        this.data = [
+            { 
+                name: "Consumer Prices Index including owner occupiers' housing costs (CPIH)",
+                lastUpdated: "Updated today at 8:44am",
+                id: "cpih"
+            },
+            { 
+                name: "Annual Summary of Hours Worked, Place of Work by Local Authority",
+                lastUpdated: "Updated yesterday at 3:15pm",
+                id: "ashe"
+            },
+            { 
+                name: "Population estimates for UK, England and Wales, Scotland and Northern Ireland",
+                lastUpdated: "Updated 16 May 2018 at 5:45pm",
+                id: "popest"
+            },
+            { 
+                name: "International Passenger Survey 4.01, citizenship group by sex by age by country of last or next residence",
+                lastUpdated: "Updated 1 May 2018 at 1:45pm",
+                id: "ips"
+            },
+            { 
+                name: "Personal crime by accomodation",
+                lastUpdated: "Updated 29 April 2018 at 10:45am",
+                id: "pca"
+            },
+        ]
+    }
+
+    getDatasetName(id) {
+        return this.data.find(dataset => {
+            return dataset.id === id
+        })
+    }
+
+    getDatasetNamesAndLastUpdated() {
+        return this.data.map(dataset => {
+            return dataset
+        })
+    }
+}
